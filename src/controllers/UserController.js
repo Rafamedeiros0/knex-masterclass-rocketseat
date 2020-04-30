4112a5c4c8b38d9ce8944f5fe448e8bb5947d320
Rafael Medeiros <32855003+Rafamedeiros0@users.noreply.github.com>
2020-04-30 13:16:38 -0300

    add express.json, edit UserController add method create user and middler on express and try catch on Controllers

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -5,5 +5,21 @@ module.exports = {
     const results = await knex('users')
 
     return res.json(results)
+  },
+  async create(req, res, next) {
+    
+    try {
+      const { username } = req.body
+
+      await knex('users').insert({
+        username
+      })
+
+      //return res.json()
+      return res.status(201).send()
+    } catch (error){
+      next(error)
+    }
+
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,5 +3,6 @@ const routes = express.Router()
 const UserController = require('./controllers/UserController')
 
 routes.get('/users', UserController.index)
+routes.post('/users', UserController.create)
     
 module.exports = routes
