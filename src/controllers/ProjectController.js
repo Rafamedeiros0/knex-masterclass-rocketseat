75dc3546cdf872a56c5efd0ad1cb865f3dd59a0a
Rafael Medeiros <32855003+Rafamedeiros0@users.noreply.github.com>
2020-04-30 14:44:00 -0300

    add pagination and add X-Total-Count send for header

--- src/controllers/ProjectController.js
+++ src/controllers/ProjectController.js
@@ -3,9 +3,13 @@ const knex = require('../database')
 module.exports = {
   async index(req, res, next) {
     try{
-      const { user_id } = req.query
+      const { user_id, page = 1} = req.query
 
       const query = knex('projects')
+        .limit(5)
+        .offset((page-1) * 5)
+
+      const countObj = knex('projects').count()
 
       if (user_id){
         query
@@ -13,8 +17,13 @@ module.exports = {
           .join('users', 'users.id', '=', 'projects.user_id')
           .select('projects.*', 'users.username')
 
+        countObj
+          .where({ user_id })
       }
       
+      const [count] = await countObj
+      res.header('X-Total-Count', count['count'])
+
       const results = await query
 
       return res.json(results)
@@ -22,4 +31,19 @@ module.exports = {
       next(error)
     }
   },
+  async create(req, res, next) {
+    try {
+      const { title, user_id } = req.body
+
+      await knex('projects').insert({
+        title,
+        user_id
+      })
+
+      //return res.json()
+      return res.status(201).send()
+    } catch (error){
+      next(error)
+    }
+  }
 }
